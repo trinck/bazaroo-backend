95e73213e541efe4dcfc5101f30ac8d1a94299a8
trinck <g.mouloungui_ibiatsi@mundiapolis.ma>
2024-11-04 22:40:38 +0100

    update env angular

--- ui-angular/src/environments/environment.prod.ts
+++ ui-angular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://193.203.191.159:88',  // URL de l'API de production
+  apiUrl: 'https://193.203.191.159:8888',  // URL de l'API de production
 };
