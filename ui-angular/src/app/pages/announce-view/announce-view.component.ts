48b06ea74cb9ff87c59c39fcb477a7b401996f73
trinck <g.mouloungui_ibiatsi@mundiapolis.ma>
2024-11-05 13:19:28 +0100

    update preview media sociaux

--- ui-angular/src/app/pages/announce-view/announce-view.component.ts
+++ ui-angular/src/app/pages/announce-view/announce-view.component.ts
@@ -5,6 +5,7 @@ import {Announce} from "../../models/Announce";
 import {ShortText} from "../../models/ShortText";
 import {CheckUnit} from "../../models/CheckUnit";
 import {Meta, Title} from "@angular/platform-browser";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-announce-view',
@@ -16,11 +17,19 @@ export class AnnounceViewComponent implements OnInit{
   id! : string;
   announces!: Announce[] ;
   announce! : Announce;
-  fieldsDeep0 = ['SHORT_TEXT', 'TEXT', 'BOOLEAN','RADIO']
+  url!:string;
+  fieldsDeep0 = ['SHORT_TEXT', 'TEXT', 'BOOLEAN','RADIO'];
   constructor(private activatedRoute: ActivatedRoute, private announcesService: AnnouncesService, private meta:Meta,private title:Title) {
      this.activatedRoute.paramMap.subscribe(value => {
        this.id = value.get("id")!
+        this.url = activatedRoute.root.snapshot.url.join("/");
+
+
      })
+
+    activatedRoute.url.subscribe(value => {
+      this.url = environment.apiUrl+"/"+value.join("/");
+    })
 
   }
 
@@ -28,21 +37,9 @@ export class AnnounceViewComponent implements OnInit{
 
      this.announcesService.getAnnounce(this.id).subscribe(value => {
           this.announce = value as Announce;
+          this.setPreviewParams();
      });
-    this.title.setTitle( this.announce.title as string);
-    this.meta.addTags([
-      { name: 'description', content: this.announce.description as string },
-      { property: 'og:title', content: this.announce.title as string },
-      { property: 'og:description', content: this.announce.description as string },
-      { property: 'og:image', content: this.announce.medias?.at(0)?.url as string },
-      { property: 'og:url', content: 'URL_de_l\'annonce' },
-      { property: 'og:type', content: 'website' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:title', content: this.announce.title as string },
-      { name: 'twitter:description', content: this.announce.description as string },
-      { name: 'twitter:image', content: this.announce.medias?.at(0)?.url as string},
-      { name: 'twitter:url', content: 'URL_de_l\'annonce' }
-    ]);
+
   }
 
 
@@ -68,6 +65,23 @@ export class AnnounceViewComponent implements OnInit{
     }).at(0);
 
 
+  }
+
+  setPreviewParams(){
+    this.title.setTitle( this.announce.title as string);
+    this.meta.addTags([
+      { name: 'description', content: this.announce.description as string },
+      { property: 'og:title', content: this.announce.title as string },
+      { property: 'og:description', content: this.announce.description as string },
+      { property: 'og:image', content: this.announce.medias?.at(0)?.url as string },
+      { property: 'og:url', content: this.url },
+      { property: 'og:type', content: 'website' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: this.announce.title as string },
+      { name: 'twitter:description', content: this.announce.description as string },
+      { name: 'twitter:image', content: this.announce.medias?.at(0)?.url as string},
+      { name: 'twitter:url', content: this.url }
+    ]);
   }
 
 }
